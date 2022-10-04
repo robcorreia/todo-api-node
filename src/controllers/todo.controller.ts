4b0c73678197ae002261db6be5aad7bcab98c06f
Robson <robccorreia@gmail.com>
2022-10-04 15:24:21 -0300

    feat: add routes

---
+++ src/controllers/todo.controller.ts
@@ -0,0 +1,10 @@
+import { Request, Response } from "express";
+import { Todo } from "../models/Todo";
+
+export const all = async () => {};
+
+export const add = async () => {};
+
+export const update = async () => {};
+
+export const remove = async () => {};
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,8 @@
-import express, { Request, Response, ErrorRequestHandler } from 'express';
-import path from 'path';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import apiRoutes from './routes/api';
+import express, { Request, Response, ErrorRequestHandler } from "express";
+import path from "path";
+import dotenv from "dotenv";
+import cors from "cors";
+import apiRoutes from "./routes/api";
 
 dotenv.config();
 
@@ -10,23 +10,23 @@ const server = express();
 
 server.use(cors());
 
-server.use(express.static(path.join(__dirname, '../public')));
+server.use(express.static(path.join(__dirname, "../public")));
 server.use(express.urlencoded({ extended: true }));
 
-server.get('/ping', (req: Request, res: Response) => res.json({ pong: true }));
+server.get("/ping", (req: Request, res: Response) => res.json({ pong: true }));
 
-server.use(apiRoutes);
+server.use("/api", apiRoutes);
 
 server.use((req: Request, res: Response) => {
-    res.status(404);
-    res.json({ error: 'Endpoint não encontrado.' });
+  res.status(404);
+  res.json({ error: "Endpoint não encontrado." });
 });
 
 const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
-    res.status(400); // Bad Request
-    console.log(err);
-    res.json({ error: 'Ocorreu algum erro.' });
-}
+  res.status(400); // Bad Request
+  console.log(err);
+  res.json({ error: "Ocorreu algum erro." });
+};
 server.use(errorHandler);
 
 server.listen(process.env.PORT);
