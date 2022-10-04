f53c6f34517eb4481a320dc1066dbae691ee8bc2
Robson <robccorreia@gmail.com>
2022-10-04 16:10:03 -0300

    feat: create new task controlle

--- src/controllers/todo.controller.ts
+++ src/controllers/todo.controller.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from "express";
+import e, { Request, Response } from "express";
 import { Todo } from "../models/Todo";
 
 export const all = async (req: Request, res: Response) => {
@@ -6,7 +6,20 @@ export const all = async (req: Request, res: Response) => {
   res.json({ tasks });
 };
 
-export const add = async () => {};
+export const add = async (req: Request, res: Response) => {
+  if (req.body.title) {
+    let newTask = await Todo.create({
+      title: req.body.title,
+      done: req.body.done ? true : false,
+    });
+
+    res.status(201).json({ item: newTask });
+  } else {
+    res.json({
+      error: "Dados não enviados.",
+    });
+  }
+};
 
 export const update = async () => {};
 
